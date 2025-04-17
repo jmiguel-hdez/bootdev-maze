b4954abe061f29c31cd15aae3dfbe4143ce9e9ca
miguel <jmiguel.hdez@gmail.com>
2025-04-17 10:21:03 -0500

    progress on maze lsn05 Cell Class

--- graphics.py
+++ graphics.py
@@ -40,4 +40,27 @@ class Line():
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width = 2)
 
+class Cell():
+    def __init__(self, x1, y1, x2, y2, win:Window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+    
+    def draw(self):
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1,self._y1),Point(self._x2,self._y1)))
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1,self._y2),Point(self._x2,self._y2)))
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1,self._y1),Point(self._x1,self._y2)))
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2,self._y1),Point(self._x2,self._y2)))
+
+
 
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from graphics import Window
-from graphics import Line,Point
+from graphics import Line,Point,Cell
 
 def main():
     win = Window(800,600)
-    p1 = Point(0,0)
-    p2 = Point(800,600)
-    line = Line(p1, p2)
-    win.draw_line(line,"red")
-    p1 = Point(0,600)
-    p2 = Point(800,0)
-    line = Line(p1, p2)
-    win.draw_line(line,"black")
+    c1 = Cell(2, 2, 20, 20, win)
+    c1.draw()
+    c2 = Cell(50,50, 100, 100, win)
+    c2.draw()
+    c3 = Cell(150, 150, 200, 200, win)
+    c3.has_bottom_wall = False
+    c3.draw()
+    c4 = Cell(210, 210, 260, 260, win)
+    c4.has_top_wall = False
+    c4.draw()
+    c5 = Cell(270, 270, 300, 300, win)
+    c5.has_left_wall = False
+    c5.draw()
+    c5 = Cell(310, 310, 360, 360, win)
+    c5.has_right_wall = False
+    c5.draw()
     win.wait_for_close()
 
 
