c259901ab4fa7c7e7db2f2f4962d00aeb366c58a
miguel <jmiguel.hdez@gmail.com>
2025-04-19 17:49:41 -0500

    separate cell to different file and move coordinates to draw method

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
+from Cell import Cell
 from graphics import Window
-from graphics import Line,Point,Cell
+from graphics import Line,Point
 
 def main():
     win = Window(800,600)
-    c1 = Cell(2, 2, 20, 20, win)
-    c1.draw()
-    c2 = Cell(50,50, 100, 100, win)
-    c2.draw()
-    c3 = Cell(150, 150, 200, 200, win)
+    c1 = Cell(win)
+    c1.draw(2, 2, 20, 20)
+    c2 = Cell(win)
+    c2.draw(50,50, 100, 100)
+    c3 = Cell(win)
     c3.has_bottom_wall = False
-    c3.draw()
-    c4 = Cell(210, 210, 260, 260, win)
+    c3.draw(150, 150, 200, 200)
+    c4 = Cell(win)
     c4.has_top_wall = False
-    c4.draw()
-    c5 = Cell(270, 270, 300, 300, win)
+    c4.draw(210, 210, 260, 260)
+    c5 = Cell(win)
     c5.has_left_wall = False
-    c5.draw()
-    c5 = Cell(310, 310, 360, 360, win)
+    c5.draw(270, 270, 300, 300)
+    c5 = Cell(win)
     c5.has_right_wall = False
-    c5.draw()
+    c5.draw(310, 310, 360, 360)
     win.wait_for_close()
 
 
