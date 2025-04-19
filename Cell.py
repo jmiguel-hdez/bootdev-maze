d77213b9e51704c010fc81f616f1819525b016e0
miguel <jmiguel.hdez@gmail.com>
2025-04-19 18:20:30 -0500

    draw_move method

--- Cell.py
+++ Cell.py
@@ -1,8 +1,8 @@
 from graphics import Line, Point, Window
 
 
-class Cell():
-    def __init__(self, win:Window):
+class Cell:
+    def __init__(self, win: Window):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -20,10 +20,32 @@ class Cell():
         self._y2 = y2
 
         if self.has_top_wall:
-            self._win.draw_line(Line(Point(self._x1,self._y1),Point(self._x2,self._y1)))
+            self._win.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            )
         if self.has_bottom_wall:
-            self._win.draw_line(Line(Point(self._x1,self._y2),Point(self._x2,self._y2)))
+            self._win.draw_line(
+                Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            )
         if self.has_left_wall:
-            self._win.draw_line(Line(Point(self._x1,self._y1),Point(self._x1,self._y2)))
+            self._win.draw_line(
+                Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            )
         if self.has_right_wall:
-            self._win.draw_line(Line(Point(self._x2,self._y1),Point(self._x2,self._y2)))
+            self._win.draw_line(
+                Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            )
+
+    def draw_move(self, to_cell, undo=False):
+        color = "gray" if undo else "red"
+        c1_center_x = ((self._x2 - self._x1) / 2) + self._x1
+        c1_center_y = ((self._y2 - self._y1) / 2) + self._y1
+        c1_center = Point(c1_center_x,c1_center_y)
+
+        c2_center_x = ((to_cell._x2 - to_cell._x1) / 2) + to_cell._x1
+        c2_center_y = ((to_cell._y2 - to_cell._y1) / 2) + to_cell._y1
+        c2_center = Point(c2_center_x,c2_center_y)
+        line = Line(c1_center, c2_center)
+        self._win.draw_line(line,color)
+
+
--- main.py
+++ main.py
@@ -4,22 +4,31 @@ from graphics import Line,Point
 
 def main():
     win = Window(800,600)
+
     c1 = Cell(win)
     c1.draw(2, 2, 20, 20)
+
     c2 = Cell(win)
     c2.draw(50,50, 100, 100)
+
     c3 = Cell(win)
     c3.has_bottom_wall = False
     c3.draw(125, 125, 200, 200)
+
     c4 = Cell(win)
     c4.has_top_wall = False
     c4.draw(225, 225, 250, 250)
+
     c5 = Cell(win)
     c5.has_left_wall = False
     c5.draw(300, 300, 500, 500)
-    c5 = Cell(win)
-    c5.has_right_wall = False
-    c5.draw(510, 510, 600, 600)
+
+    c6 = Cell(win)
+    c6.has_right_wall = False
+    c6.draw(510, 510, 600, 600)
+
+    c3.draw_move(c5,False)
+
     win.wait_for_close()
 
 
